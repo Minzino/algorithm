53479a2c68aaa9a19963576d530238c10307c540
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-07-21 14:44:28 +0900

    인프런 자바 알고리즘0104

---
+++ src/String/problem0104.java
@@ -0,0 +1,40 @@
+package String;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class problem0104 {
+
+	public ArrayList<String> solution(int n, String[] str) {
+		ArrayList<String> answer = new ArrayList<>();
+		for (String x : str) {
+			char[] s = x.toCharArray();
+			int left = 0;
+			int right = x.length() - 1;
+			while (left < right) {
+				char tmp = s[left];
+				s[left] = s[right];
+				s[right] = tmp;
+				left++;
+				right--;
+			}
+			String tmp = String.valueOf(s);
+			answer.add(tmp);
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		problem0104 T = new problem0104();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[] str = new String[n];
+		for (int i = 0; i < n; i++) {
+			str[i] = sc.next();
+		}
+		for (String x : T.solution(n, str)) {
+			System.out.println(x);
+		}
+	}
+
+}
