281ab05857babf6efc2c9f5ce28caca7ba28d496
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-07-27 21:18:57 +0900

    인프런 자바 알고리즘 0107다른 풀이, 0108

--- src/String/problem0107.java
+++ src/String/problem0107.java
@@ -4,14 +4,30 @@ import java.util.Scanner;
 
 public class problem0107 {
 
+//	public String solution(String str) {
+//		String answer = "YES";
+//		String upperStr = str.toUpperCase();
+//		int strLength = str.length();
+//		for (int i = 0; i < strLength / 2; i++) {
+//			if (upperStr.charAt(i) != upperStr.charAt(strLength - i - 1)){
+//				answer ="NO";
+//			}
+//		}
+//		return answer;
+//	}
+//
+//	public static void main(String[] args) {
+//		problem0107 T = new problem0107();
+//		Scanner sc = new Scanner(System.in);
+//		String str = sc.next();
+//		System.out.println(T.solution(str));
+//	}
+
 	public String solution(String str) {
-		String answer = "YES";
-		String upperStr = str.toUpperCase();
-		int strLength = str.length();
-		for (int i = 0; i < strLength / 2; i++) {
-			if (upperStr.charAt(i) != upperStr.charAt(strLength - i - 1)){
-				answer ="NO";
-			}
+		String answer = "NO";
+		String tmp = new StringBuilder(str).reverse().toString();
+		if (str.equalsIgnoreCase(tmp)) {
+			answer = "YES";
 		}
 		return answer;
 	}
@@ -20,7 +36,8 @@ public class problem0107 {
 		problem0107 T = new problem0107();
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
-		System.out.println(T.solution(str));
+		System.out.print(T.solution(str));
+
 	}
 
 }
