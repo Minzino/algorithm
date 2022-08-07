291e9345e80a9bd7534dd1cf2d0dff4fbe25cb98
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-08-07 16:41:31 +0900

    인프런 자바 알고리즘 0208(배열:등수구하기)

---
+++ src/Array/Problem0208.java
@@ -0,0 +1,37 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Problem0208 {
+
+	public int[] solution(int students, int[] scores) {
+		int[] answer = new int[students];
+		for (int i = 0; i < students; i++) {
+			int cnt = 1;
+			for (int j = 0; j < scores.length; j++) {
+				if (scores[j] > scores[i]) {
+					cnt++;
+				}
+			}
+			answer[i] = cnt;
+
+		}
+		return answer;
+	}
+
+
+	public static void main(String[] args) {
+		Problem0208 T = new Problem0208();
+		Scanner sc = new Scanner(System.in);
+
+		int students = sc.nextInt();
+		int[] scores = new int[students];
+
+		for (int i = 0; i < students; i++) {
+			scores[i] = sc.nextInt();
+		}
+		for (int x : T.solution(students, scores)) {
+			System.out.print(x + " ");
+		}
+	}
+}
