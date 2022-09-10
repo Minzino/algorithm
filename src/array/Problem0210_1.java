c46b1d80183a9f93404e1423acbc2134f9a3f7c7
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-09-10 18:15:51 +0900

    :memo: 배열10번문제 복습

--- src/array/Problem0210_1.java
+++ src/array/Problem0210_1.java
@@ -4,67 +4,29 @@ import java.util.Scanner;
 
 public class Problem0210_1 {
 
+	int[] dx = {-1, 0, 1, 0};
+	int[] dy = {0, 1, 0, -1};
 	public int solution(int number, int[][] map) {
-		int result = 0;
-		int[][] colCheck = new int[number][number];
-		int[][] rowCheck = new int[number][number];
-		for (int i = 0; i < number; i++) {
-			for (int j = 1; j < number; j++) {
-				if (map[i][j - 1] < map[i][j]) {
-					rowCheck[i][j - 1] = 0;
-					rowCheck[i][j] = 1;
+		int answer = 0;
 
-				} else {
-					if (j > 1) {
-						if (rowCheck[i][j - 2] == 1 && rowCheck[i][j - 1] == 0) {
-							rowCheck[i][j - 1] = 0;
-							rowCheck[i][j] = 0;
-							continue;
-						}
-						if (rowCheck[i][j - 2] == 0 && rowCheck[i][j - 1] == 0) {
-							rowCheck[i][j - 1] = 0;
-							rowCheck[i][j] = 0;
-							continue;
-						}
-					}
-					rowCheck[i][j - 1] = 1;
-					rowCheck[i][j] = 0;
-				}
-			}
-		}
-		for (int i = 0; i < number; i++) {
-			for (int j = 1; j < number; j++) {
-				if (map[j - 1][i] < map[j][i]) {
-					colCheck[j - 1][i] = 0;
-					colCheck[j][i] = 1;
-				} else {
-					if (j > 1) {
-						if (colCheck[j - 2][i] == 1 && colCheck[j - 1][i] == 0) {
-							colCheck[j - 1][i] = 0;
-							colCheck[j][i] = 0;
-							continue;
-						}
-						if (colCheck[j - 2][i] == 0 && colCheck[j - 1][i] == 0) {
-							colCheck[j - 1][i] = 0;
-							colCheck[j][i] = 0;
-							continue;
-						}
-
-					}
-					colCheck[j - 1][i] = 1;
-					colCheck[j][i] = 0;
-				}
-			}
-		}
 		for (int i = 0; i < number; i++) {
 			for (int j = 0; j < number; j++) {
-				if (colCheck[i][j] == rowCheck[i][j] && rowCheck[i][j] == 1
-					&& colCheck[i][j] == 1) {
-					result++;
+				boolean flag=true;
+				for (int k = 0; k < dx.length; k++) {
+					int nx = i + dx[k];
+					int ny = j + dy[k];
+					if (nx >= 0 && nx < number && ny >= 0 && ny < number
+						&& map[nx][ny] >= map[i][j]) {
+						flag=false;
+						break;
+					}
+				}
+				if(flag) {
+					answer++;
 				}
 			}
 		}
-		return result;
+		return answer;
 	}
 
 	public static void main(String[] args) {
