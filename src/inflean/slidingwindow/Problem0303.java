85153666d778f8725627cbb3176f90d32ddd791f
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-09-13 11:57:21 +0900

    :sparkles: 연속된 날짜의 최대 매출 구하기(sliding window) - 인프런 자바 알고리즘

---
+++ src/inflean/slidingwindow/Problem0303.java
@@ -0,0 +1,44 @@
+package inflean.slidingwindow;
+
+import java.util.Scanner;
+
+public class Problem0303 {
+
+	public int solution(int days, int continuous, int[] sales) {
+		int tmp = 0;
+		int p1 = 0;
+		int p2 = continuous;
+		for (int j = 0; j < continuous; j++) {
+			tmp += sales[j];
+		}
+
+		int max = tmp;
+
+		for (int i = continuous; i < days; i++) {
+			tmp += sales[p2] - sales[p1];
+			if (max < tmp) {
+				max = tmp;
+			}
+			p1++;
+			p2++;
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Problem0303 T = new Problem0303();
+
+		int days = sc.nextInt();
+		int continuous = sc.nextInt();
+		int[] sales = new int[days];
+
+		for (int i = 0; i < days; i++) {
+			sales[i] = sc.nextInt();
+		}
+
+		System.out.println(T.solution(days, continuous, sales));
+
+	}
+
+}
