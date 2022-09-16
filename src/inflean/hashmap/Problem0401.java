14a87c777c5d7ad026d9f7bb2924d4d20102bf63
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-09-16 14:45:56 +0900

    :sparkles: 학급 회장(hashmap) - 인프런 자바 알고리즘

---
+++ src/inflean/hashmap/Problem0401.java
@@ -0,0 +1,31 @@
+package inflean.hashmap;
+
+import java.util.*;
+
+class Problem0401 {
+
+	public char solution(int n, String s) {
+		char answer = ' ';
+		HashMap<Character, Integer> map = new HashMap<>();
+		for (char x : s.toCharArray()) {
+			map.put(x, map.getOrDefault(x, 0) + 1);
+		}
+
+		int max = Integer.MIN_VALUE;
+		for (char key : map.keySet()) {
+			if (map.get(key) > max) {
+				max = map.get(key);
+				answer = key;
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Problem0401 t = new Problem0401();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String str = sc.next();
+		System.out.println(t.solution(n, str));
+	}
+}
