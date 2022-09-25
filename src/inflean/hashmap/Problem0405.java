2312acacabc2a8a03f7e5d18224ee5d86c8aad26
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-09-25 15:07:11 +0900

    :memo: K번째 큰 수(트리셋) - 주석제거 및 클래스이름 변경

--- src/inflean/hashmap/Problem0405.java
+++ src/inflean/hashmap/Problem0405.java
@@ -1,7 +1,7 @@
 package inflean.hashmap;
 
 import java.util.*;
-class Main {
+class Problem0405 {
 	public int solution(int[] arr, int n, int k){
 		int answer=-1;
 		TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
@@ -13,13 +13,8 @@ class Main {
 			}
 		}
 		int cnt=0;
-		//Tset.remove(143);
-		//System.out.println(Tset.size());
-		//System.out.println("first : "+ Tset.first());
-		//System.out.println("last : "+ Tset.last());
 
 		for(int x : Tset){
-			//System.out.println(x);
 			cnt++;
 			if(cnt==k) return x;
 		}
@@ -27,7 +22,7 @@ class Main {
 	}
 
 	public static void main(String[] args){
-		Main T = new Main();
+		Problem0405 T = new Problem0405();
 		Scanner kb = new Scanner(System.in);
 		int n=kb.nextInt();
 		int k=kb.nextInt();
