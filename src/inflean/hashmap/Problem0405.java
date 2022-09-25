69086f90f8103691fc921331c977ce455022d98c
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-09-25 17:12:53 +0900

    :memo: K번째 큰 수(트리셋) - 보기좋게 코드 정렬

--- src/inflean/hashmap/Problem0405.java
+++ src/inflean/hashmap/Problem0405.java
@@ -1,34 +1,39 @@
 package inflean.hashmap;
 
 import java.util.*;
+
 class Problem0405 {
-	public int solution(int[] arr, int n, int k){
-		int answer=-1;
-		TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
-		for(int i=0; i<n; i++){
-			for(int j=i+1; j<n; j++){
-				for(int l=j+1; l<n; l++){
-					Tset.add(arr[i]+arr[j]+arr[l]);
+
+	public int solution(int[] arr, int n, int k) {
+		int answer = -1;
+		TreeSet<Integer> treeSet = new TreeSet<>(Collections.reverseOrder());
+
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				for (int l = j + 1; l < n; l++) {
+					treeSet.add(arr[i] + arr[j] + arr[l]);
 				}
 			}
 		}
-		int cnt=0;
+		int cnt = 0;
 
-		for(int x : Tset){
+		for (int x : treeSet) {
 			cnt++;
-			if(cnt==k) return x;
+			if (cnt == k) {
+				return x;
+			}
 		}
 		return answer;
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Problem0405 T = new Problem0405();
 		Scanner kb = new Scanner(System.in);
-		int n=kb.nextInt();
-		int k=kb.nextInt();
-		int[] arr=new int[n];
-		for(int i=0; i<n; i++){
-			arr[i]=kb.nextInt();
+		int n = kb.nextInt();
+		int k = kb.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = kb.nextInt();
 		}
 		System.out.println(T.solution(arr, n, k));
 	}
