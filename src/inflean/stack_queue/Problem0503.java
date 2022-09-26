b2748c1058f0118b0ec0c8a6cf57d5ea9dbb288c
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-09-26 17:10:06 +0900

    :sparkles: 인형 뽑기(스택 - 3) - 인프런 알고리즘

---
+++ src/inflean/stack_queue/Problem0503.java
@@ -0,0 +1,50 @@
+package inflean.stack_queue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Problem0503 {
+
+	public int solution(int[][] board, int[] moves) {
+		int answer = 0;
+		Stack<Integer> stack = new Stack<>();
+		stack.push(0);
+
+		for (int move : moves) {
+			for (int j = 0; j < board.length; j++) {
+				if (board[j][move - 1] != 0) {
+					if (stack.peek() == board[j][move - 1]) {
+						stack.pop();
+						answer += 2;
+					} else {
+						stack.push(board[j][move - 1]);
+					}
+					board[j][move - 1] = 0;
+					break;
+				}
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Problem0503 t = new Problem0503();
+
+		int number = sc.nextInt();
+		int[][] board = new int[number][number];
+
+		for (int i = 0; i < number; i++) {
+			for (int j = 0; j < number; j++) {
+				board[i][j] = sc.nextInt();
+			}
+		}
+
+		int move = sc.nextInt();
+		int[] moves = new int[move];
+
+		System.out.println(t.solution(board, moves));
+
+	}
+
+}
