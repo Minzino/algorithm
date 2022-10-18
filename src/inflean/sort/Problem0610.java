da421d7f49cc234853de223a7fcfb3e971b7e3be
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-10-18 19:34:22 +0900

    :sparkles: 인프런 알고리즘 - 마구간 정하기(이분탐색)

---
+++ src/inflean/sort/Problem0610.java
@@ -0,0 +1,47 @@
+package inflean.sort;
+
+import java.util.*;
+
+class Problem0610 {
+
+	public int count(int[] arr, int dist) {
+		int cnt = 1;
+		int ep = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] - ep >= dist) {
+				cnt++;
+				ep = arr[i];
+			}
+		}
+		return cnt;
+	}
+
+	public int solution(int n, int c, int[] arr) {
+		int answer = 0;
+		Arrays.sort(arr);
+		int lt = 1;
+		int rt = arr[n - 1];
+		while (lt <= rt) {
+			int mid = (lt + rt) / 2;
+			if (count(arr, mid) >= c) {
+				answer = mid;
+				lt = mid + 1;
+			} else {
+				rt = mid - 1;
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Problem0610 t = new Problem0610();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int c = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(t.solution(n, c, arr));
+	}
+}
