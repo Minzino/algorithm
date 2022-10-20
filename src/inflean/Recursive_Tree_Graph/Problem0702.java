730b08e802f0c21cb8d55c05e689eba9e2dd1cd4
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-10-20 13:50:01 +0900

    :sparkles: 인프런 알고리즘 - 재귀

---
+++ src/inflean/Recursive_Tree_Graph/Problem0702.java
@@ -0,0 +1,23 @@
+package inflean.Recursive_Tree_Graph;
+
+public class Problem0702 {
+
+	public void DFS(int n) {
+		if (n == 0) {
+			return;
+		} else {
+			DFS(n / 2);
+			System.out.print(n % 2);
+		}
+	}
+
+	public void solution(int n) {
+		DFS(n);
+	}
+
+	public static void main(String[] args) {
+		Problem0702 t = new Problem0702();
+		t.solution(11);
+	}
+}
+
---
+++ src/inflean/Recursive_Tree_Graph/Problem0703.java
@@ -0,0 +1,17 @@
+package inflean.Recursive_Tree_Graph;
+
+class Problem0703 {
+
+	public int DFS(int n) {
+		if (n == 1) {
+			return 1;
+		} else {
+			return n * DFS(n - 1);
+		}
+	}
+
+	public static void main(String[] args) {
+		Problem0703 t = new Problem0703();
+		System.out.println(t.DFS(5));
+	}
+}
