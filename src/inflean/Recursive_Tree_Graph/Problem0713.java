4e6b047990438a6a43315d8ab60fc5de815c51e5
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-11-03 14:13:56 +0900

    :sparkles: 인프런 알고리즘 - 경로탐색(인접리스트, ArrayList)

---
+++ src/inflean/Recursive_Tree_Graph/Problem0713.java
@@ -0,0 +1,51 @@
+package inflean.Recursive_Tree_Graph;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Problem0713 {
+
+	static int n, m, answer = 0;
+	static ArrayList<ArrayList<Integer>> graph;
+	static int[] ch;
+
+	public void DFS(int v) {
+		if (v == n) {
+			answer++;
+		} else {
+			for (int nv : graph.get(v)) {
+				if (ch[nv] == 0) {
+					ch[nv] = 1;
+					DFS(nv);
+					ch[nv] = 0;
+				}
+			}
+		}
+	}
+
+
+	public static void main(String[] args) {
+		Problem0713 t = new Problem0713();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		graph = new ArrayList<ArrayList<Integer>>();
+
+		for (int i = 0; i <= n; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+
+		ch = new int[n + 1];
+		for (int i = 0; i < m; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			graph.get(a).add(b);
+		}
+
+		ch[1] = 1;
+		t.DFS(1);
+		System.out.println(answer);
+
+	}
+
+}
