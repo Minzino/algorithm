529b3b6ef3603ef4cdfc67bf420c2dbf499ca85b
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-11-24 14:00:59 +0900

    :sparkles:  입출력 문제 풀이

---
+++ baekjoon/BJ8595/Main.java
@@ -0,0 +1,34 @@
+package BJ8595;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        StringBuilder tmp = new StringBuilder();
+        long sum = 0;
+
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                tmp.append(c);
+            } else {
+                if (tmp.length() > 0) {
+                    sum += Long.parseLong(tmp.toString());
+                    tmp = new StringBuilder();
+                }
+            }
+        }
+        if (tmp.length() > 0) {
+            sum += Long.parseLong(tmp.toString());
+            tmp = new StringBuilder();
+        }
+        System.out.println(sum);
+
+    }
+
+}
