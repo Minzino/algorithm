bc3455935834bb7836c7dc980ba8b45e0948bb7b
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-12-04 19:34:51 +0900

    :sparkles: 알고리즘 - 인프런(DFS_BFS) 토마토 익힘

--- inflean/dfs_bfs/problem0812/Main.java
+++ inflean/dfs_bfs/problem0812/Main.java
@@ -1,5 +1,8 @@
 package dfs_bfs.problem0812;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 class Point {
@@ -35,16 +38,18 @@ class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Main t = new Main();
-        Scanner sc = new Scanner(System.in);
-        m = sc.nextInt();
-        n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         board = new int[n][m];
         dis = new int[n][m];
         for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                board[i][j] = sc.nextInt();
+                board[i][j] = Integer.parseInt(st.nextToken());
                 if (board[i][j] == 1) {
                     Q.offer(new Point(i, j));
                 }
