259c39a718b5af5f08b46442314277fff0d3799e
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-12-07 18:33:37 +0900

    :sparkles: 알고리즘 - 인프런(Union & Find, PriorityQueue) - 원더랜드

---
+++ inflean/greedy/problem0908/Main.java
@@ -0,0 +1,59 @@
+package greedy.problem0908;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class Edge implements Comparable<Edge> {
+
+    public int vex;
+    public int cost;
+
+    Edge(int vex, int cost) {
+        this.vex = vex;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge ob) {
+        return this.cost - ob.cost;
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        int[] ch = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            graph.get(a).add(new Edge(b, c));
+            graph.get(b).add(new Edge(a, c));
+        }
+        int answer = 0;
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>();
+        priorityQueue.offer(new Edge(1, 0));
+        while (!priorityQueue.isEmpty()) {
+            Edge tmp = priorityQueue.poll();
+            int ev = tmp.vex;
+            if (ch[ev] == 0) {
+                ch[ev] = 1;
+                answer += tmp.cost;
+                for (Edge ob : graph.get(ev)) {
+                    if (ch[ob.vex] == 0) {
+                        priorityQueue.offer(new Edge(ob.vex, ob.cost));
+                    }
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
