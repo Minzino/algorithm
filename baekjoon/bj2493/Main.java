836dbd119a04170cb5352a4dda3ce4227f64ec93
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-12-22 14:50:22 +0900

    :sparkles: 알고리즘 - 스택(백준2493): 탑

---
+++ baekjoon/bj2493/Main.java
@@ -0,0 +1,32 @@
+package bj2493;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Stack<int[]> stack = new Stack<>();
+        for(int i = 1; i <= n; i++) {
+            int top = Integer.parseInt(st.nextToken());
+            while(!stack.isEmpty()) {
+                if(stack.peek()[1] >= top) {
+                    sb.append(stack.peek()[0]).append(" ");
+                    break;
+                }
+                stack.pop();
+            }
+            if(stack.isEmpty()) {
+                sb.append("0 ");
+            }
+            stack.push(new int[] {i, top});
+        }
+        System.out.println(sb);
+    }
+}
