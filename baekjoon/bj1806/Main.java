c8561ca5f40e53d7dbdd90ed92b3da96f532dceb
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-01-17 14:00:43 +0900

    :sparkles: 알고리즘(투포인터) - 부분합: 1806

---
+++ baekjoon/bj1806/Main.java
@@ -0,0 +1,46 @@
+package bj1806;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int lt = 0;
+        int rt = 0;
+
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        while (lt <= n && rt <= n) {
+            if (sum >= s) {
+                min = Math.min(min, rt - lt);
+            }
+            if (sum < s) {
+                sum += arr[rt++];
+            } else {
+                sum -= arr[lt++];
+            }
+        }
+
+        if (min == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(min);
+        }
+    }
+
+}
