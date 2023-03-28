fdc37ecf77d47de1ef90b179f2d48ebc3625bc73
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-03-28 17:46:40 +0900

    feat: 백준 1926 그림 bfs 재풀이

--- baekjoon/bj1926/Main.java
+++ baekjoon/bj1926/Main.java
@@ -3,7 +3,6 @@ package bj1926;
 import java.util.*;
 import java.io.*;
 
-
 class Point {
 
     int x;
@@ -17,18 +16,25 @@ class Point {
 
 public class Main {
 
+
+    static int row, col;
+    static int[][] board;
+    static boolean[][] visit;
+    static Queue<Point> queue;
+    static int area;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int row = Integer.parseInt(st.nextToken());
-        int col = Integer.parseInt(st.nextToken());
 
-        int[][] board = new int[row][col];
-        boolean[][] visit = new boolean[row][col];
-        Queue<Point> queue = new LinkedList<>();
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
 
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, -1, 0, 1};
+        board = new int[row][col];
+        visit = new boolean[row][col];
 
         for (int i = 0; i < row; i++) {
             st = new StringTokenizer(br.readLine());
@@ -38,7 +44,6 @@ public class Main {
         }
 
         int count = 0;
-        int area;
         int max = 0;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
@@ -46,32 +51,36 @@ public class Main {
                     continue;
                 }
                 count++;
-                queue.offer(new Point(i, j));
-                visit[i][j] = true;
-                area = 0;
-                while (!queue.isEmpty()) {
-                    Point point = queue.poll();
-                    area++;
-                    for (int k = 0; k < 4; k++) {
-                        int nx = point.x + dx[k];
-                        int ny = point.y + dy[k];
-                        if (nx < 0 || nx >= row || ny < 0 || ny >= col) {
-                            continue;
-                        }
-                        if (board[nx][ny] == 1 && !visit[nx][ny]) {
-                            queue.offer(new Point(nx, ny));
-                            visit[nx][ny] = true;
-                        }
-                    }
-                }
+                bfs(i, j);
                 if (area > max) {
                     max = area;
                 }
             }
-
         }
         System.out.println(count);
         System.out.println(max);
+    }
+
+    static void bfs(int startX, int startY) {
+        queue = new LinkedList<>();
+        queue.offer(new Point(startX, startY));
+        visit[startX][startY] = true;
+        area = 0;
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+            area++;
+            for (int i = 0; i < 4; i++) {
+                int nx = point.x + dx[i];
+                int ny = point.y + dy[i];
+                if (nx < 0 || nx >= row || ny < 0 || ny >= col) {
+                    continue;
+                }
+                if (board[nx][ny] == 1 && !visit[nx][ny]) {
+                    queue.offer(new Point(nx, ny));
+                    visit[nx][ny] = true;
+                }
+            }
+        }
     }
 
 }
