ee7760fa60417231548694c77c0c62b14e41ff6f
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-04-27 00:50:04 +0900

    refactor: 변수명 수정

--- baekjoon/bj14503/Main.java
+++ baekjoon/bj14503/Main.java
@@ -9,8 +9,8 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static int N, M;
-    public static int[][] map;
-    public static int cnt = 0;
+    public static int[][] roomMap;
+    public static int cleanedCount = 0;
     public static int[] dr = {-1, 0, 1, 0}; // 북,동,남,서
     public static int[] dc = {0, 1, 0, -1};
 
@@ -21,45 +21,45 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
+        roomMap = new int[N][M];
 
         st = new StringTokenizer(br.readLine());
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
+        int row = Integer.parseInt(st.nextToken());
+        int col = Integer.parseInt(st.nextToken());
+        int direction = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                roomMap[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        clean(r, c, d);
+        cleanRoom(row, col, direction);
 
-        bw.write(cnt + "\n");
+        bw.write(cleanedCount + "\n");
         br.close();
         bw.flush();
         bw.close();
     }
 
-    public static void clean(int row, int col, int direction) {
+    public static void cleanRoom(int row, int col, int direction) {
         // 1. 현재 위치를 청소한다.
-        if (map[row][col] == 0) {
-            map[row][col] = 2;
-            cnt++;
+        if (roomMap[row][col] == 0) {
+            roomMap[row][col] = 2;
+            cleanedCount++;
         }
 
-        // 2. 왼쪽방향부터 차례대로 탐색을 진행한다.
+        // 2. 왼쪽 방향부터 차례대로 탐색을 진행한다.
         boolean flag = false;
-        int origin = direction;
+        int originalDirection = direction;
         for (int i = 0; i < 4; i++) {
-            int next_d = (direction + 3) % 4;
-            int next_r = row + dr[next_d];
-            int next_c = col + dc[next_d];
+            int nextDirection = (direction + 3) % 4;
+            int nextRow = row + dr[nextDirection];
+            int nextCol = col + dc[nextDirection];
 
-            if (next_r > 0 && next_c > 0 && next_r < N && next_c < M) {
-                if (map[next_r][next_c] == 0) {   // 아직 청소하지 않은 공간이라면
-                    clean(next_r, next_c, next_d);
+            if (nextRow > 0 && nextCol > 0 && nextRow < N && nextCol < M) {
+                if (roomMap[nextRow][nextCol] == 0) {   // 아직 청소하지 않은 공간이라면
+                    cleanRoom(nextRow, nextCol, nextDirection);
                     flag = true;
                     break;
                 }
@@ -69,13 +69,13 @@ public class Main {
 
         // 네 방향 모두 청소가 되어있거나 벽인 경우
         if (!flag) {
-            int next_d = (origin + 2) % 4;
-            int next_br = row + dr[next_d];
-            int next_bc = col + dc[next_d];
+            int nextDirection = (originalDirection + 2) % 4;
+            int nextBackRow = row + dr[nextDirection];
+            int nextBackCol = col + dc[nextDirection];
 
-            if (next_br > 0 && next_bc > 0 && next_br < N && next_bc < M) {
-                if (map[next_br][next_bc] != 1) {
-                    clean(next_br, next_bc, origin); // 바라보는 방향 유지한 채 후진
+            if (nextBackRow > 0 && nextBackCol > 0 && nextBackRow < N && nextBackCol < M) {
+                if (roomMap[nextBackRow][nextBackCol] != 1) {
+                    cleanRoom(nextBackRow, nextBackCol, originalDirection); // 바라보는 방향 유지한 채 후진
                 }
             }
         }
