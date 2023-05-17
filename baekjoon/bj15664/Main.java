9f09f22b13e7d85bca5a8d2a3ee9fd9c5c8ec5d0
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-05-17 22:27:01 +0900

    feat: 백트래킹 - 백준(15664) N과 M(10)

---
+++ baekjoon/bj15664/Main.java
@@ -0,0 +1,55 @@
+package bj15664;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr, nums;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        nums = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        dfs(0, 0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int lastSelected = -1;
+        for (int i = start; i < N; i++) {
+            if (lastSelected != nums[i]) {
+                arr[depth] = nums[i];
+                lastSelected = nums[i];
+                dfs(i + 1, depth + 1);
+            }
+        }
+    }
+}
