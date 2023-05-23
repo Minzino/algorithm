7df323c2c925749a6f60e37a1bb84624d5174088
Meenzino <86910955+Minzino@users.noreply.github.com>
2023-05-23 18:37:55 +0900

    feat: bfs 백준(7562) - 나이트의 이동

--- baekjoon/bj7562/Main.java
+++ baekjoon/bj7562/Main.java
@@ -1,9 +1,69 @@
 package bj7562;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
+    static int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
+    static int l;
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            l = Integer.parseInt(br.readLine());
+            int[][] board = new int[l][l];
+            boolean[][] visited = new boolean[l][l];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int endX = Integer.parseInt(st.nextToken());
+            int endY = Integer.parseInt(st.nextToken());
+
+            bfs(x, y, endX, endY, board, visited);
+            sb.append(board[endX][endY]).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    private static void bfs(int x, int y, int endX, int endY, int[][] board, boolean[][] visited) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(x, y));
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+            if (point.x == endX && point.y == endY) {
+                break;
+            }
+            for (int i = 0; i < 8; i++) {
+                int nx = point.x + dx[i];
+                int ny = point.y + dy[i];
+                if (nx >= 0 && nx < l && ny >= 0 && ny < l && !visited[nx][ny]) {
+                    queue.offer(new Point(nx, ny));
+                    visited[nx][ny] = true;
+                    board[nx][ny] = board[point.x][point.y] + 1;
+                }
+            }
+        }
+    }
+
+    static class Point {
+
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
